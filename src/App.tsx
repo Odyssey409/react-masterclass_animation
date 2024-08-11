35a66ac1ea655fb284730cb2f4dbcd6a079b0068
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-08-12 02:02:46 +0900

    section#8.6 Gestures part Two
     drag의 dragConstraints 중 dragSnapToOrigin, dragElastic을 배우고 ref 사용법을 다시 학습함

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -12,10 +13,20 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const BiggerBox = styled(motion.div)`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
 `;
 
 const BoxVariants = {
@@ -25,15 +36,21 @@ const BoxVariants = {
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={BoxVariants}
-        whileHover="Hover"
-        whileTap="Tap"
-        whileDrag="Drag"
-      ></Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragConstraints={biggerBoxRef}
+          dragSnapToOrigin
+          dragElastic={0.5}
+          variants={BoxVariants}
+          whileHover="Hover"
+          whileTap="Tap"
+          whileDrag="Drag"
+        ></Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
