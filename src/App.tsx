c2d1cd5373371fccd18e60e9083f6d692ad01cbc
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-08-12 16:43:09 +0900

    section#8.8 MotionValues Gestures part Two
    framer-motion의 useTransform 기능을 학습함 useTransform을 사용하면 위치 값에 따른 크기 변화 효과 등을 부여할 수 있음

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion, useMotionValue } from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import { useEffect, useRef } from "react";
 
 const Wrapper = styled.div`
@@ -26,13 +26,13 @@ const BoxVariants = {
 
 function App() {
   const x = useMotionValue(0);
+  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
   useEffect(() => {
     x.onChange(() => console.log(x.get()));
   }, [x]);
   return (
     <Wrapper>
-      <button onClick={() => x.set(200)}>click me</button>
-      <Box style={{ x }} drag="x" dragSnapToOrigin></Box>
+      <Box style={{ x, scale: scale }} drag="x" dragSnapToOrigin></Box>
     </Wrapper>
   );
 }
