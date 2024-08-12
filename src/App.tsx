fb1e34a8e62eb649a60f81aac533ec3a2d8228e1
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-08-12 19:29:24 +0900

    section#8.12 Slider part One
    슬라이더를 만드는 실습을 함. 그러나 아직은 prev 실행 시 next와 같은 방향 (오른쪽에서 나오는 방향) 이기에 다음 강의에서 개선 예정.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import {
 } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import { start } from "repl";
+import { set } from "react-hook-form";
+import { transcode } from "buffer";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -16,6 +18,7 @@ const Wrapper = styled(motion.div)`
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
@@ -26,30 +29,54 @@ const Box = styled(motion.div)`
   position: absolute;
   top: 100px;
   box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.1), 0px 10px 20px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
 `;
-const BoxVariants = {
-  initial: { opacity: 0, scale: 0 },
-  visible: { opacity: 1, scale: 1, rotateZ: 360 },
-  leaving: { opacity: 0, scale: 0, y: 50 },
+
+const boxVariants = {
+  invisible: {
+    x: 500,
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: { duration: 1 },
+  },
+  exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 1 } },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => {
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click me</button>
       <AnimatePresence>
-        {" "}
-        {showing ? (
-          <Box
-            variants={BoxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) =>
+          item === visible ? (
+            <Box
+              key={item}
+              variants={boxVariants}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+            >
+              {item}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
+      <button onClick={nextPlease}>Next</button>
+      <button onClick={prevPlease}>Prev</button>
     </Wrapper>
   );
 }
